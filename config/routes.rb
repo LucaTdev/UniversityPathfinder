b4bffe582c74bfe66d2f42b14ed186b8cda7163d
Alessandro Nardi <nardi.1853941@studenti.uniroma1.it>
2025-08-21 13:00:59 +0200

    Creazione file profilo e file sedi
    Ho creato i file profilo e sedi, e ho iniziato a dare una forma al profilo, giusto per farmi un'idea

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "home/index"
+
+  get "home/profilo"
   #root 'home#index' #setta homepage come root
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
