7059f45f23d0bedbe8dc62d870802ec4d4b33edb
Alessandro Nardi <nardi.1853941@studenti.uniroma1.it>
2025-08-21 13:05:51 +0200

    Creazione profilo

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get "home/index"
 
   get "home/profilo"
+
+  get "home/sedi"
   #root 'home#index' #setta homepage come root
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
