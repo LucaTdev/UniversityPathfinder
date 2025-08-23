c70e3536173038615fc46fc2c25f1394f9b2ca2c
Alessio Trionfera <t.alessio03@gmail.com>
2025-08-23 13:22:08 +0200

    Creazione Pagina news

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
   get 'registration', to: 'users#new'
 
   get 'home/registrazione'
+
+  get "home/meteo"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
