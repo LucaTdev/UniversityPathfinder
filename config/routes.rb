455e4a6bc036a48ce424d6ac7d7aa0f0eb5c9bac
Slayfer21 <t.francesco14@gmail.com>
2025-08-24 16:21:55 +0200

    meteo locale + notifiche meteo
    sono tutte API di open meteo

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
 
   get 'home/registrazione'
 
-  get "home/meteo"
+  get "home/meteo", to: "home#meteo"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
