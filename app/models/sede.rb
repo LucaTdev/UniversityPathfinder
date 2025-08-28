a99f48e566e7ea64ba6941c2d6b3d79254777c46
Alessandro Nardi <nardi.1853941@studenti.uniroma1.it>
2025-08-28 20:15:08 +0200

    Aggiungto home/supporto e latitudine e longitudine

--- app/models/sede.rb
+++ app/models/sede.rb
@@ -1,5 +1,6 @@
 class Sede < ApplicationRecord
     self.table_name = "sedi"
-    validates :nome, :indirizzo, presence: true
+    validates :nome, :indirizzo, :latitudine, :longitudine, presence: true
     validates :edifici, presence: true
+    validates :latitudine, :longitudine, numericality:true
 end
