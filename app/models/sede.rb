6cc29c8ada60bcda0cf584f5721f8ca38cfcc7a9
Alessandro Nardi <nardi.1853941@studenti.uniroma1.it>
2025-08-28 21:47:35 +0200

    Lat/Long
    Ora quando inserisci/modifichi una sede la lat e la long vengono inserite correttamente nel database, con tanto di indicatore e funzione di percorso

--- app/models/sede.rb
+++ app/models/sede.rb
@@ -1,6 +1,5 @@
 class Sede < ApplicationRecord
     self.table_name = "sedi"
-    validates :nome, :indirizzo, :latitudine, :longitudine, presence: true
+    validates :nome, :indirizzo, :lat, :long, presence: true
     validates :edifici, presence: true
-    validates :latitudine, :longitudine, numericality:true
 end
